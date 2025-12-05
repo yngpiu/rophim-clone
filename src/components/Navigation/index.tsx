d561e44c8125b3eadc439eac0900931f3a6c6903
Trịnh Hoài Đức <anhduc08768@gmail.com>
2025-12-05 10:37:56 +0700

    feat: cải tiến Navigation component và cập nhật styles
    - Thêm chức năng mở/đóng cho danh sách danh mục trong Navigation.
    - Cập nhật styles cho Navigation để hỗ trợ hiển thị dropdown và cải thiện trải nghiệm người dùng.
    - Thêm hàm color-opacity và mixin truncate vào các file Sass để sử dụng trong styles.

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,7 @@
 import { faCaretDown, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { categoryList } from '@/data/navigation';
@@ -14,26 +15,62 @@ type NavigationProps = {
 };
 
 const Navigation = ({ menuIsOpen }: NavigationProps) => {
+  const [activeCategory, setActiveCategory] = useState<string>('');
+
+  const handleClick = (categoryId: string) => {
+    console.log(categoryId, activeCategory);
+    setActiveCategory(prev => {
+      if (prev === categoryId) return '';
+      return categoryId;
+    });
+  };
+
   return (
-    <nav className={cx('navigation', menuIsOpen && 'navigation--opened')}>
-      <Link to={{ pathname: 'login' }} className={cx('navigation__login')}>
+    <nav className={cx('nav', { 'nav--opened': menuIsOpen })}>
+      <Link to={{ pathname: 'login' }} className={cx('nav__login')}>
         <FontAwesomeIcon icon={faUser} />
         Thành viên
       </Link>
 
-      <ul className={cx('navigation__list')}>
+      <ul className={cx('nav__list')}>
         {categoryList.map(category => (
-          <li key={category.id} className={cx('navigation__item')}>
-            <Link to={{ pathname: category.slug }} className={cx('navigation__link')}>
-              {category.name}{' '}
-              {category?.subcategories && category.subcategories.length > 0 && (
-                <FontAwesomeIcon icon={faCaretDown} />
-              )}
-            </Link>
+          <li
+            key={category.id}
+            className={cx('nav__item')}
+            onClick={() => handleClick(category?.id ?? '')}
+          >
+            {category?.subcategories && category.subcategories.length > 0 ? (
+              <span className={cx('nav__link')}>
+                {category.name} <FontAwesomeIcon icon={faCaretDown} />
+              </span>
+            ) : (
+              <Link to={{ pathname: category.slug }} className={cx('nav__link')}>
+                {category.name}
+              </Link>
+            )}
+            {category?.subcategories && category.subcategories.length > 0 && (
+              <div
+                className={cx('nav__dropdown', {
+                  'nav__dropdown--active': activeCategory === category?.id,
+                  'nav__dropdown--single-column': category.id === '5',
+                })}
+              >
+                <ul className={cx('nav__sublist')}>
+                  {category.subcategories.map(subcategory => (
+                    <li key={subcategory.id} className={cx('nav__subitem')}>
+                      <Link to={{ pathname: subcategory.slug }} className={cx('nav__sublink')}>
+                        {subcategory.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </li>
         ))}
       </ul>
     </nav>
   );
 };
+
 export default Navigation;
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,184 +1,184 @@
 export const categoryList = [
   {
-    id: 1,
+    id: '1',
     name: 'Phim lẻ',
     slug: 'phim-le',
   },
   {
-    id: 2,
+    id: '2',
     name: 'Phim bộ',
     slug: 'phim-bo',
   },
   {
-    id: 3,
+    id: '3',
     name: 'Thể loại',
     subcategories: [
       {
-        id: 28,
+        id: '28',
         name: 'Hành Động',
         slug: 'hanh-dong',
       },
       {
-        id: 12,
+        id: '12',
         name: 'Phiêu Lưu',
         slug: 'phieu-luu',
       },
       {
-        id: 16,
+        id: '16',
         name: 'Hoạt Hình',
         slug: 'hoat-hinh',
       },
       {
-        id: 35,
+        id: '35',
         name: 'Hài',
         slug: 'hai',
       },
       {
-        id: 80,
+        id: '80',
         name: 'Hình Sự',
         slug: 'hinh-su',
       },
       {
-        id: 99,
+        id: '99',
         name: 'Tài Liệu',
         slug: 'tai-lieu',
       },
       {
-        id: 18,
+        id: '18',
         name: 'Chính Kịch',
         slug: 'chinh-kich',
       },
       {
-        id: 10751,
+        id: '10751',
         name: 'Gia Đình',
         slug: 'gia-dinh',
       },
       {
-        id: 14,
+        id: '14',
         name: 'Giả Tượng',
         slug: 'gia-tuong',
       },
       {
-        id: 36,
+        id: '36',
         name: 'Lịch Sử',
         slug: 'lich-su',
       },
       {
-        id: 27,
+        id: '27',
         name: 'Kinh Dị',
         slug: 'kinh-di',
       },
       {
-        id: 10402,
+        id: '10402',
         name: 'Nhạc',
         slug: 'nhac',
       },
       {
-        id: 9648,
+        id: '9648',
         name: 'Bí Ẩn',
         slug: 'bi-an',
       },
       {
-        id: 10749,
+        id: '10749',
         name: 'Lãng Mạn',
         slug: 'lang-man',
       },
       {
-        id: 878,
+        id: '878',
         name: 'Khoa Học Viễn Tưởng',
         slug: 'khoa-hoc-vien-tuong',
       },
+
       {
-        id: 10770,
-        name: 'Chương Trình Truyền Hình',
-        slug: 'chuong-trinh-truyen-hinh',
-      },
-      {
-        id: 53,
+        id: '53',
         name: 'Gây Cấn',
         slug: 'gay-can',
       },
       {
-        id: 10752,
+        id: '10770',
+        name: 'Chương Trình Truyền Hình',
+        slug: 'chuong-trinh-truyen-hinh',
+      },
+      {
+        id: '10752',
         name: 'Chiến Tranh',
         slug: 'chien-tranh',
       },
       {
-        id: 37,
+        id: '37',
         name: 'Miền Tây',
         slug: 'mien-tay',
       },
     ],
   },
   {
+    id: '5',
     name: 'Quốc Gia',
     subcategories: [
-      [
-        {
-          id: 'GB',
-          name: 'Anh',
-          slug: 'anh',
-        },
-        {
-          id: 'CA',
-          name: 'Canada',
-          slug: 'canada',
-        },
-        {
-          id: 'KR',
-          name: 'Hàn Quốc',
-          slug: 'han-quoc',
-        },
-        {
-          id: 'HK',
-          name: 'Hồng Kông',
-          slug: 'hong-kong',
-        },
-        {
-          id: 'US',
-          name: 'Mỹ',
-          slug: 'my',
-        },
-        {
-          id: 'JP',
-          name: 'Nhật Bản',
-          slug: 'nhat-ban',
-        },
-        {
-          id: 'FR',
-          name: 'Pháp',
-          slug: 'phap',
-        },
-        {
-          id: 'TH',
-          name: 'Thái Lan',
-          slug: 'thai-lan',
-        },
-        {
-          id: 'CN',
-          name: 'Trung Quốc',
-          slug: 'trung-quoc',
-        },
-        {
-          id: 'AU',
-          name: 'Úc',
-          slug: 'uc',
-        },
-        {
-          id: 'TW',
-          name: 'Đài Loan',
-          slug: 'dai-loan',
-        },
-        {
-          id: 'DE',
-          name: 'Đức',
-          slug: 'duc',
-        },
-      ],
+      {
+        id: 'GB',
+        name: 'Anh',
+        slug: 'anh',
+      },
+      {
+        id: 'CA',
+        name: 'Canada',
+        slug: 'canada',
+      },
+      {
+        id: 'KR',
+        name: 'Hàn Quốc',
+        slug: 'han-quoc',
+      },
+      {
+        id: 'HK',
+        name: 'Hồng Kông',
+        slug: 'hong-kong',
+      },
+      {
+        id: 'US',
+        name: 'Mỹ',
+        slug: 'my',
+      },
+      {
+        id: 'JP',
+        name: 'Nhật Bản',
+        slug: 'nhat-ban',
+      },
+      {
+        id: 'FR',
+        name: 'Pháp',
+        slug: 'phap',
+      },
+      {
+        id: 'TH',
+        name: 'Thái Lan',
+        slug: 'thai-lan',
+      },
+      {
+        id: 'CN',
+        name: 'Trung Quốc',
+        slug: 'trung-quoc',
+      },
+      {
+        id: 'AU',
+        name: 'Úc',
+        slug: 'uc',
+      },
+      {
+        id: 'TW',
+        name: 'Đài Loan',
+        slug: 'dai-loan',
+      },
+      {
+        id: 'DE',
+        name: 'Đức',
+        slug: 'duc',
+      },
     ],
   },
   {
-    id: 4,
+    id: '4',
     name: 'Diễn viên',
     slug: 'dien-vien',
   },
