df3be028b9832c2e346efd42b206779a67736857
Trịnh Hoài Đức <anhduc08768@gmail.com>
2025-12-06 21:55:14 +0700

    feat: cập nhật hàm chuyển đổi thời gian phim
    - Đổi tên hàm từ convertSecondsToFilmDuration thành convertMinutesToFilmDuration để phản ánh chính xác chức năng.
    - Thay đổi logic để chuyển đổi từ phút sang định dạng giờ và phút, cải thiện khả năng hiển thị.
    - Thêm điều kiện để xử lý trường hợp không có giờ hoặc phút còn lại.

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,10 @@
-export const convertSecondsToFilmDuration = (seconds: number) => {
-  if (!seconds || seconds === 0) return '';
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  return `${hours}h ${minutes}m`;
+export const convertMinutesToFilmDuration = (minutes: number) => {
+  if (!minutes || minutes === 0) return '';
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = Math.floor(minutes % 60);
+
+  if (hours === 0) return `${minutes}m`;
+  if (remainingMinutes === 0) return `${hours}h`;
+
+  return `${hours}h ${remainingMinutes}m`;
 };
